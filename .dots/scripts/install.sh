61d2dbd15ee876f405a913edfdfdbe246b8a3dd5
samu-delucas <smlucas7@gmail.com>
2021-06-27 13:46:29 +0200

    change alias dotscfg -> .git and rename repo directory .dotscfg -> .dotsrepo

--- .dots/scripts/install.sh
+++ .dots/scripts/install.sh
@@ -9,23 +9,23 @@
 
 ### Cloning the repository
 # 1. Avoid recursion errors
-echo ".dotscfg" >> .gitignore
+echo ".dotsrepo" >> .gitignore
 # 2. Clone the repo
-git clone --bare https://github.com/samu-delucas/dots.git $HOME/.dotscfg
+git clone --bare https://github.com/samu-delucas/dots.git $HOME/.dotsrepo
 # 3. Set an alias for this session
 # (the same alias is included in ~/.dots/shell/.aliasrc)
-alias dotscfg='/usr/bin/git --git-dir=$HOME/.dotscfg/.git --work-tree=$HOME'
+alias .git='/usr/bin/git --git-dir=$HOME/.dotsrepo/.git --work-tree=$HOME'
 # 4. Don't show untracked files (we only want to track the ones that we add)
-dotscfg config --local status.showUntrackedFiles no
+.git config --local status.showUntrackedFiles no
 # 5. Get the actual files (I think lol)
 # If there is an error with existing files, backup the files and try again
-dotscfg checkout
+.git checkout
 if [ $? = 0 ]; then
   echo "Checked out correctly.";
   else
     echo "Backing up pre-existing dot files.";
     mkdir -p .config-backup
-    dotscfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    .git checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
 fi;
 
 ### Setting paths
